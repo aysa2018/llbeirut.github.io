bb91bb27ae43c826fe2b8bfbc0b09f1234d46b08
aysa2018 <123475912+aysa2018@users.noreply.github.com>
2023-11-26 23:14:44 +0400

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,3 +2,6 @@ $('.frame').click(function() {
     // Redirect to larger image URL
     window.location.href = 'ff42a90e-f94c-434b-b299-4ca850c35c55_t.png';
 });
+$(document).ready(function() {
+    $('.image-popup').magnificPopup({type:'image'});
+});
